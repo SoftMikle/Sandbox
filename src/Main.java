a7864c1eae6124d752a32f5281acc12a10cff41a
Mikle Savchenko <58853610+SoftMikle@users.noreply.github.com>
2020-02-09 13:51:30 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("FU");
         var list = new ArrayList<LinkedList<String>>();
+        list.foreach(System.out::println);
     }
 }
